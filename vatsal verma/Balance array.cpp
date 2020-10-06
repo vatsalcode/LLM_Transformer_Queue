c2de51e83743ba55a9f48da16c273a334e0e3d1e
Rohan Pradhan <58914647+coderrohan14@users.noreply.github.com>
2020-10-06 14:52:49 +0530

    Update Balance array.cpp
    Reduced an extra traversal ...

--- vatsal verma/Balance array.cpp
+++ vatsal verma/Balance array.cpp
@@ -11,9 +11,7 @@ public:
        int sum2=0;
        for(int i=0;i<n/2;i++){
            sum1+=a[i];
-       }
-       for(int i=n/2;i<n;i++){
-           sum2+=a[i];
+           sum2+=a[(n/2)+i];
        }
        
        return abs(sum1-sum2);
