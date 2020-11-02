b02253573320c54c7a94f13726a7c2ab83a89ac9
Rohan Pradhan <58914647+coderrohan14@users.noreply.github.com>
2020-11-02 10:20:53 +0530

    Create AndPlusOrLunchtime.java

---
+++ coderrohan14/AndPlusOrLunchtime.java
@@ -0,0 +1,82 @@
+/***
+* Author - coderrohan14
+* October Lunchtime 2020
+* Problem link - https://www.codechef.com/LTIME89B/problems/ANDOR
+***/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    public static void main (String[] args) throws IOException
+	 {
+		FastReader sc = new FastReader();
+		long t = sc.nextLong();
+		StringBuffer ans = new StringBuffer("");
+		while(t-->0) {
+			String s = sc.nextLine();
+			ans.append("0 " + s + "\n");
+		}
+		System.out.println(ans);
+	}
+	
+	  static class FastReader 
+	    { 
+	        BufferedReader br; 
+	        StringTokenizer st; 
+	  
+	        public FastReader() 
+	        { 
+	            br = new BufferedReader(new
+	                     InputStreamReader(System.in)); 
+	        } 
+	  
+	        String next() 
+	        { 
+	            while (st == null || !st.hasMoreElements()) 
+	            { 
+	                try
+	                { 
+	                    st = new StringTokenizer(br.readLine()); 
+	                } 
+	                catch (IOException  e) 
+	                { 
+	                    e.printStackTrace(); 
+	                } 
+	            } 
+	            return st.nextToken(); 
+	        } 
+	  
+	        int nextInt() 
+	        { 
+	            return Integer.parseInt(next()); 
+	        } 
+	  
+	        long nextLong() 
+	        { 
+	            return Long.parseLong(next()); 
+	        } 
+	  
+	        double nextDouble() 
+	        { 
+	            return Double.parseDouble(next()); 
+	        } 
+	  
+	        String nextLine() 
+	        { 
+	            String str = ""; 
+	            try
+	            { 
+	                str = br.readLine(); 
+	            } 
+	            catch (IOException e) 
+	            { 
+	                e.printStackTrace(); 
+	            } 
+	            return str; 
+	        } 
+	    } 
+}
