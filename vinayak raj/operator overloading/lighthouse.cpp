471b9f84ead17702a4f298396242fa03eacdc6da
Vraj_codes <vinayakrajbusiness@gmail.com>
2020-11-08 00:29:40 +0530

    Add files via upload

---
+++ vinayak raj/operator overloading/lighthouse.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+class time{
+private:
+    float hr,mins,secs;
+public:
+    void operator >>(int ob)
+    {
+        cin>>hr>>mins>>secs;
+    }
+    void operator <<(int ob)
+    {
+        cout<<hr<<" hours "<<mins<<" minutes "<<secs<<" seconds ";
+    }
+};
+
+
+int main()
+{
+    time t;
+    float a=3;
+    t.operator >>(a);
+    t.operator <<(a);
+
+    return 0;
+}
