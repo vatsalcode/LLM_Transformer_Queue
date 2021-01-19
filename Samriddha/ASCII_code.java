584140986c7e98ed2f89fc8d48372122a199c0b7
Samriddha Basu <73631825+samriddha-basu-cloud@users.noreply.github.com>
2021-01-19 20:04:15 +0530

    Create ASCII_code.java

---
+++ Samriddha/ASCII_code.java
@@ -0,0 +1,12 @@
+public class ASCII_code   
+  {  
+    public static void main(String[] args)   
+    {  
+      char ch1 = 'a';  
+      char ch2 = 'b';  
+      int asciivalue1 = ch1;  
+      int asciivalue2 = ch2;  
+      System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);  
+      System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);  
+    }  
+  }
