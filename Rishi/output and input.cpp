a9272370d5ce83ed851373958cb89641e4ee0888
Rishi838 <77577908+Rishi838@users.noreply.github.com>
2021-01-20 20:59:59 +0530

    Create output and input.cpp

---
+++ Rishi/output and input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+   int main ()
+{
+    int num1,num2;
+    cout<<"Enter the value of num1: \n";
+    cin>>num1;
+ cout<<"Enter the value of num2: \n";
+    cin>>num2;
+
+    cout<<"The sum of  two numbers is \n"<<  num1 + num2;
+
+    return 0;
+}
