26c4de3c23b2942f76e813798ba9aac52dd75efb
Tanish2002 <tanishkhare@gmail.com>
2021-01-20 20:57:29 +0530

    Update type_casting.nim

--- Tanish2002/Learn_Nim/type_casting.nim
+++ Tanish2002/Learn_Nim/type_casting.nim
@@ -1,1 +1,16 @@
+# These types are inferred.
+var x = 5 # int
+var y = "foo" # string
 
+# Assigning a value of a different type will result in a compile-time error.
+x = y
+
+
+
+var x = int(1.0 / 3) # type conversion
+
+var y: seq[int] = @[] # empty seq needs type specification
+
+var z = "Foobar"
+proc ffi(foo: ptr array[6, char]) = echo repr(foo)
+ffi(cast[ptr array[6, char]](addr z[0]))
