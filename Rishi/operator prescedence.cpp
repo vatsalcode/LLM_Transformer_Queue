524c1858dd3e63094b51145ae67cb840f10a2fd3
Rishi838 <77577908+Rishi838@users.noreply.github.com>
2021-01-22 10:41:56 +0530

    Create operator prescedence.cpp

---
+++ Rishi/operator prescedence.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+float a, b;
+cout<<"enter the value of a"<<endl;
+cin>>a;
+cout<<"enter the value of b"<<endl;
+cin>>b;
+float c=(((a*b)/10)+((a*3)/b));
+cout<<"The value of c is (((a*b)/10)+((a*3)/b)):";
+cout <<c;
+return 0;
+}
