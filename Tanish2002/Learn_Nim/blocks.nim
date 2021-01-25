6217b6a20e6ea4a1f2c5f6193683e8291d2a49fd
Tanish2002 <tanishkhare@gmail.com>
2021-01-26 02:08:56 +0530

    Update blocks.nim

--- Tanish2002/Learn_Nim/blocks.nim
+++ Tanish2002/Learn_Nim/blocks.nim
@@ -8,4 +8,15 @@ block:
   echo "This line, on the other hand, is always output"
 
 
+#-------------------------------------------------------
+block outer:
+  for i in 0..2000:
+    for j in 0..2000:
+      if i+j == 3145:
+        echo i, ", ", j
+        break outer
+
+let b = 3
+block:
+  let b = "3"  # shadowing is probably a dumb idea
 
