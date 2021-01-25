e2292ab3a02b4ee87920aecda1f7a581759d95a0
Tanish2002 <tanishkhare@gmail.com>
2021-01-26 02:05:29 +0530

    Update first-class-functions.nim

--- Tanish2002/Learn_Nim/first-class-functions.nim
+++ Tanish2002/Learn_Nim/first-class-functions.nim
@@ -7,3 +7,18 @@ echo powersOfTwo.filter(proc (x: int): bool = x > 32)
 
 proc greaterThan32(x: int): bool = x > 32
 echo powersOfTwo.filter(greaterThan32)
+
+# -----------------------------------------------------
+import sugar
+
+# sugar provides a "->" macro that simplifies writing type
+# declarations, e.x. (char) -> char
+proc map(str: string, fun: (char) -> char): string =
+  for c in str:
+    result &= fun(c)
+
+# sugar also provides a "=>" macro for the actual lambda
+# value
+echo "foo".map((c) => char(ord(c) + 1))
+# the following code is exactly equvilent:
+echo "foo".map(proc (c: char): char = char(ord(c) + 1))
