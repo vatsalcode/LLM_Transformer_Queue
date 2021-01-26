4ca0f55a62c4ea601d9279b6795756e61029ece5
Tanish2002 <tanishkhare@gmail.com>
2021-01-26 21:34:49 +0530

    Update primitive-types.nim

--- Tanish2002/Learn_Nim/primitive-types.nim
+++ Tanish2002/Learn_Nim/primitive-types.nim
@@ -3,3 +3,10 @@ let
   b: uint8 = 0b1111_1111 # Works
   d = 0xFF # type is int
   c: uint8 = 256 # Compile time error
+
+#-----------------------------
+#Precedence rules are the same as in most other languages, but instead of ^, &, |, >>, <<, the xor, and, or, shr, shl operators are used, respectively.
+let
+  a: int = 2
+  b: int = 4
+echo 4/2
