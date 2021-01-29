07960f80be8dc1b262d49f34270046c18b7ac8ec
Samriddha Basu <73631825+samriddha-basu-cloud@users.noreply.github.com>
2021-01-29 23:49:42 +0530

    Create Const_name.java

---
+++ Samriddha/Const_name.java
@@ -0,0 +1,16 @@
+class Name {
+  String name;
+
+  public Name() {
+    name = "Samriddha";
+  }
+}
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Name obj = new Name();
+    System.out.println("Name = " + obj.name);
+  }
+}
