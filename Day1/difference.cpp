81624847f876c05fae763fbed7023e2294ad4e67
Dhruv Pandey <76779907+thenextgoogler@users.noreply.github.com>
2021-01-31 23:33:37 +0530

    Create difference.cpp

---
+++ Day1/difference.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a;
+int b;
+int d;
+cout<<"Enter the Greater Number";
+cin>>a;
+cout<<"Enter the Smaller Number";
+cin>>b;
+d=a-b;
+cout<<"The difference between both is"<<d;
+return 0;
+}
