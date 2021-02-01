c778a6b6ac5d4c44d6786ac9c74139fe6c251bce
Tanish2002 <tanishkhare@gmail.com>
2021-02-01 17:33:31 +0530

    Update strings.nim

--- Tanish2002/Learn_Nim/strings.nim
+++ Tanish2002/Learn_Nim/strings.nim
@@ -12,3 +12,9 @@ proc re(s: string): string = s
 
 echo r".""."
 echo re"\b[a-z]++\b"
+
+#---------------------------------------------------
+
+echo "abc"[0]
+echo "abcdefg"[0 .. 4]
+echo "abcdefg"[0 .. ^2]
