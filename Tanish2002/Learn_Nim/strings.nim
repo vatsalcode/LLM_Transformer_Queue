a216bff65c054a017140dd8393b3b97d14698391
Tanish2002 <tanishkhare@gmail.com>
2021-02-01 17:33:58 +0530

    Update strings.nim

--- Tanish2002/Learn_Nim/strings.nim
+++ Tanish2002/Learn_Nim/strings.nim
@@ -18,3 +18,15 @@ echo re"\b[a-z]++\b"
 echo "abc"[0]
 echo "abcdefg"[0 .. 4]
 echo "abcdefg"[0 .. ^2]
+
+#---------------------------------------------------
+
+import strutils
+
+var a = "hello welcome,friend"
+
+# The split proc takes a sequence of characters and splits a string based on them
+echo a.split({' ', ','})
+
+# The contains proc determines whether a string contains a substring or character
+echo a.contains("hello")
