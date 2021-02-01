21973e1ed5870885377a5ea4c731c7115caea2a7
Tanish2002 <tanishkhare@gmail.com>
2021-02-01 17:28:55 +0530

    Update distinct_types.nim

--- Tanish2002/Learn_Nim/distinct_types.nim
+++ Tanish2002/Learn_Nim/distinct_types.nim
@@ -4,3 +4,9 @@ type
 var a = 20.Dollars
 a = 25  # Doesn't compile
 a = 25.Dollars  # Works fine
+
+#--------------------------------------------------------------
+
+proc `*` *(a, b: Dollars): Dollars {.borrow.}
+proc `+` *(a, b: Dollars): Dollars {.borrow.}
+a = 20.Dollars * 20.Dollars
