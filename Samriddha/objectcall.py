7b6eaf537d8fc8768d5e3ee0bfcb85366dc906bc
Samriddha Basu <73631825+samriddha-basu-cloud@users.noreply.github.com>
2021-02-01 23:29:19 +0530

    Create objectcall.py

---
+++ Samriddha/objectcall.py
@@ -0,0 +1,9 @@
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("Samridha Basu", 18)
+p1.myfunc()
