b5e1b497dc3b6b47063d5aea8867b1e3f6f426ce
Samriddha Basu <73631825+samriddha-basu-cloud@users.noreply.github.com>
2021-02-02 23:27:28 +0530

    Create Halting.java

---
+++ Samriddha/Halting.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+    int result = sum(5, 10);
+    System.out.println(result);
+  }
+  public static int sum(int start, int end) {
+    if (end > start) {
+      return end + sum(start, end - 1);
+    } else {
+      return end;
+    }
+  }
+}
