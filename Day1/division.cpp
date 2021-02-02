4370924d1b2ea798b343d131ea895626854b872e
Dhruv Pandey <76779907+thenextgoogler@users.noreply.github.com>
2021-02-02 23:36:29 +0530

    Create division.cpp

---
+++ Day1/division.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    cout << "\n\n Divide two numbers and print:\n";
+	int a;
+	int b;
+	int resdiv;
+	a=30;
+	b=10;
+	resdiv=a/b;
+	cout << " The quotient of "<< a << " and "<<b <<" is : "<< resdiv <<"\n\n" ;
+}
