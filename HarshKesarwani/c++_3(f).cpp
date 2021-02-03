6170e8df2f7c7eee72a75d50c14ab403f4a5c5fa
Harsh Kesarwani <77579808+hkesarwani27@users.noreply.github.com>
2021-02-03 22:56:55 +0530

    Create c++_3(f).cpp

---
+++ HarshKesarwani/c++_3(f).cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+int age;
+cout<<"enter your age";
+cin>>age;
+if(age<18)
+{
+    cout<<"not eligible";
+}
+else if(age==18)
+{
+    cout<<"will come but not will be given anything";
+}
+else
+{
+    cout<<"you are eligible";
+}
+
+}
