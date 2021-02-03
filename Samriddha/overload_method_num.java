05dce4a31e5a457c7536bddb9bafeee43813125b
Samriddha Basu <73631825+samriddha-basu-cloud@users.noreply.github.com>
2021-02-03 22:57:26 +0530

    Create overload_method_num.java

---
+++ Samriddha/overload_method_num.java
@@ -0,0 +1,16 @@
+public class Main {
+  static int plusMethod(int x, int y) {
+    return x + y;
+  }
+  
+  static double plusMethod(double x, double y) {
+    return x + y;
+  }
+  
+  public static void main(String[] args) {
+    int myNum1 = plusMethod(8, 5);
+    double myNum2 = plusMethod(4.3, 6.26);
+    System.out.println("int: " + myNum1);
+    System.out.println("double: " + myNum2);
+  }
+}
