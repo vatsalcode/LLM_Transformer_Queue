6cb0ef37613d8bb1cedf401844592ff52be8b431
Samriddha Basu <73631825+samriddha-basu-cloud@users.noreply.github.com>
2021-02-10 23:21:56 +0530

    Create HarshadNumber.java

---
+++ Samriddha/HarshadNumber.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class HarshadNumber
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+         
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        int c = n, d, sum = 0;
+         
+        while(c>0)
+        {
+            d = c%10;
+            sum = sum + d;
+            c = c/10;
+        }
+         
+        if(n%sum == 0)
+            System.out.println(n+" is a Harshad Number.");
+        else
+            System.out.println(n+" is not a Harshad Number.");      
+    }
+}
