458dff02e5c4fb16cace37f9b660f7c6544eaa3c
Dhruv Pandey <76779907+thenextgoogler@users.noreply.github.com>
2021-02-12 23:10:48 +0530

    Create math.cpp

---
+++ math.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>    
+#include <cmath>  
+using namespace std;
+int main ()
+{
+  cout << "abs (10.57) = " << abs (10.57) << '\n';
+  cout << "abs (-25.63)  = " << abs (-25.63) << '\n';
+  return 0;
+}
