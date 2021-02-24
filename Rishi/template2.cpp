c141314224479f1c6e7d8e05d71083a32de7c623
Rishi Dhingra <rishidhingra04@gmail.com>
2021-02-24 18:22:03 +0530

    Create template2.cpp

---
+++ Rishi/template2.cpp
@@ -0,0 +1,33 @@
+//Templates with multiple parametres
+#include <iostream>
+using namespace std;
+
+/*
+template<class T1,class T2>
+class name{
+  body
+};
+*/
+template <class T1, class T2>
+class myClass
+{
+public:
+    T1 data1;
+    T2 data2;
+    myClass(T1 a, T2 b)
+    {
+        data1 = a;
+        data2 = b;
+    }
+    void display()
+    {
+        cout << this->data1 << this->data2;
+    }
+};
+
+int main()
+{
+    myClass<int, char> obj(1, 'c');
+    obj.display();
+    return 0;
+}
