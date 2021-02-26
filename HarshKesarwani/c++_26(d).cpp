668613dc718a89628170b33a94320eab88754329
Harsh Kesarwani <77579808+hkesarwani27@users.noreply.github.com>
2021-02-26 22:43:55 +0530

    Create c++_26(d).cpp

---
+++ HarshKesarwani/c++_26(d).cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+class base
+{
+    int data1;
+
+public:
+    int data2;
+    void setdata();
+    int getdata1();
+    int getdata2();
+};
+
+void base ::setdata()
+{
+    data1 = 10;
+    data2 = 20;
+}
+
+int base ::getdata1()
+{
+    return data1;
+}
+
+int base ::getdata2()
+{
+    return data2;
+}
+
+class derived : private base
+{
+    int data3;
+
+public:
+    void process();
+    void display();
+};
+
+void derived ::process()
+{
+    setdata();  // cannot be invoked from main fun coz derived class visibilty mode is private
+    data3 = data2 * getdata1();
+}
+
+void derived ::display()
+{
+    cout << "Value of data1 is " << getdata1() << endl
+         << "Value of data2 is " << data2 << endl
+         << "Value of data3 is " << data3 << endl;
+}
+
+int main()
+{
+    derived d;
+    d.process();
+    d.display();
+    return 0;
+} 
