2a420c206c6eacca8dfd0a49ba6a8b9a56835261
Dhruv Pandey <76779907+thenextgoogler@users.noreply.github.com>
2021-02-28 23:01:35 +0530

    Create Gpat.cpp

---
+++ DHRUV PANDEY/Gpat.cpp
@@ -0,0 +1,31 @@
+#include <iostream> 
+using namespace std; 
+  
+void pattern(int line) 
+{ 
+    int i, j; 
+    for(i = 0; i < line; i++) 
+    { 
+        for(j = 0; j < line; j++) 
+        { 
+            if((j == 1 && i != 0 && i != line - 1) || 
+              ((i == 0 || i == line - 1) && j > 1 && 
+              j < line - 2) || (i == ((line - 1) / 2) &&  
+              j > 2 && j < line - 1) || (j == line - 2 &&  
+              i != 0 && i >= ((line - 1) / 2) && i != line - 1)) 
+                printf("*"); 
+            else
+                printf( " "); 
+  
+        } 
+        printf("\n"); 
+    } 
+} 
+  
+
+int main()  
+{ 
+    int line = 7; 
+    pattern(line); 
+    return 0; 
+} 
