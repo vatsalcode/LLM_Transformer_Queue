07faabce6363288370e9464532b6641b6295196f
vaibhav-r <77624981+vaibhav-r@users.noreply.github.com>
2021-03-04 01:57:10 +0530

    Create day46.java

---
+++ day46.java
@@ -0,0 +1,11 @@
+            String s1 = br.readLine();
+            String s2 = br.readLine();
+            String s3 = br.readLine();
+            System.out.println("String: " + s3);
+            System.out.println("Double: " + Double.parseDouble(s2));
+            System.out.println("Int: " + Integer.parseInt(s1));
+            
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
