e9f1a7ddc02a13b599f40ce4859f630c91f936c4
Rishi Dhingra <rishidhingra04@gmail.com>
2021-03-06 11:55:17 +0530

    Create ESCAPE SEQUENCE.C

---
+++ Rishi/ESCAPE SEQUENCE.C
@@ -0,0 +1,13 @@
+//Escape sequence
+#include <stdio.h>
+int main()
+{
+    //Backlash sequence
+    printf("Hlo everyone \\ \n");
+    printf("Hlo everyone \\n \n");
+    //Tab character
+    printf("Tab charater \t\t It will create  a tab \n");
+    //Alarm command
+    printf("Listen to the windows sound \a");
+    return 0;
+}
