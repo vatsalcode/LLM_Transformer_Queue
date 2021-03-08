7d993a1a001e9efadeb6799921f5e2fe5bac3c70
Shreyansh Sharma <77615082+ShreyanshSh31@users.noreply.github.com>
2021-03-08 10:45:27 +0530

    Create pattern_fourth.java

---
+++ ShreyanshSh31/pattern_fourth.java
@@ -0,0 +1,23 @@
+//to print the pattern
+//1
+//22
+//333
+//4444
+//55555
+//666666
+
+class pattern_fourth
+{
+    public static void main(String args[])
+    {
+    int i,j;
+    for (i=1;i<=6;i++)
+    {
+        for(j=1;j<=i;j++)
+        System.out.print(+i);
+    System.out.println(" ");
+}
+}
+}
+
+    
