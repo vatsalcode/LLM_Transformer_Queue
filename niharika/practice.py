1791fb52c5a80bd1d5b4add6aa3d99ca8217907d
niharikab23 <77580769+niharikab23@users.noreply.github.com>
2021-03-09 19:09:07 +0530

    Create practice.py

---
+++ niharika/practice.py
@@ -0,0 +1,26 @@
+sks = 'harold'
+pos = sks.find('ol')
+print (pos)
+huh = sks.find('u')
+print(huh)
+
+line = 'hi how are you'
+print(line.startswith('h'))
+print(line.endswith('u'))
+
+stuff = 'hello\nworlds'
+print(stuff)
+
+x = ['thanks','lol','weird']
+x[2] = 'yummy'
+print(x)
+
+a = [45,87,98]
+b = [4,'hu','lmao']
+z = a+b
+print(z)
+
+ru = list()
+ru.append('yus')
+ru.append('gus')
+print(ru)
