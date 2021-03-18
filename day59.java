e1616f354a425415f9ffb870ecbfba2376d5f731
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2021-03-18 17:14:44 +0530

    Create day59.java

---
+++ day59.java
@@ -0,0 +1,15 @@
+package vaibhav;
+import java.util.*;
+public class Day1{
+	
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+    
+	int n = sc.nextInt();
+    for (int j=1; j<=n; j++) {
+    	
+    	System.out.println(j*j*j*j+2*j);
+ 
+	}
+	}
+}
