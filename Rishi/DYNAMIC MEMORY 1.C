ccc743ec131c305399aaadfadbb133c3412a57fc
Rishi Dhingra <rishidhingra04@gmail.com>
2021-03-23 20:16:51 +0530

    Create DYNAMIC MEMORY 1.C

---
+++ Rishi/DYNAMIC MEMORY 1.C
@@ -0,0 +1,38 @@
+//Dynamic Memory Allocation
+/*
+A company manages record of other companies
+Employee ID can be of any length and contain any character
+for 3 employees we have to take length of Id as input in a length integer varaiable and then you have to take ID
+as input and diplay their id on screen which is to be done dynamically
+we have to create only 1 character array
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int chars, i = 0;
+    char a, b;
+    char *ptr;
+    while (i < 3)
+    {
+        printf("Employee %d: Enter the number of characters in your Employee Id\n", i+1);
+        scanf("%d", &chars);
+        getchar();
+        printf("Enter the value of a\n");
+        scanf("%c", &a);
+        getchar();
+        printf("Enter the value of b\n");
+        scanf("%c", &b);
+        ptr = (char *) malloc((chars+1)*sizeof(char));
+        printf("Enter your Employee Id\n");
+        scanf("%s", ptr);
+        printf("Your Employee Id is %s\n",ptr );
+        free(ptr);
+        i = i + 1;
+    }
+
+    return 0;
+}
+© 2020-2021 CodeWithHarry.com
+
