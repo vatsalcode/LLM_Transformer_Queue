9d73985cac23294991ba9f0a38cc3b3f6e48471a
Bishal Kumar Ghosh <81809899+bISHAL-2001@users.noreply.github.com>
2021-04-03 20:43:57 +0530

    Create Day1Question2.py

---
+++ Bishal/Day1Question2.py
@@ -0,0 +1,12 @@
+def sortnames(name1, name2, name3):
+    x = [name1, name2, name3]
+    x.sort()
+    print("Your Names is sorted order: ")
+    for i in x:
+        print(i)
+
+
+a = input("Input name1: ")
+b = input("Input name2: ")
+c = input("Input name3: ")
+sortnames(a, b, c)
