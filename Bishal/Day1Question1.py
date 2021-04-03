fa3e138fb696e16e7d4312a6dab7dfe01a6e1213
Bishal Kumar Ghosh <81809899+bISHAL-2001@users.noreply.github.com>
2021-04-03 21:07:00 +0530

    Day1Question1

--- Bishal/Day1Question1.py
+++ Bishal/Day1Question1.py
@@ -1,1 +1,19 @@
+def names(name):
+    sta = """
+    Hello {}!
+    Welcome to our world of new innovations.   
+    """
+    print(sta.format(name))
 
+
+def altnames(name):
+    st = f"""
+    Hello {name}!
+    Welcome to our new era!
+    """
+    print(st)
+
+
+n = input("What's your name?\n")
+names(n)
+altnames(n)
