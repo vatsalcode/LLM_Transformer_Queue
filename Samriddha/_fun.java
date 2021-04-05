165854277c545836c3a6611cb6ebc63374a1b89d
Samriddha Basu <73631825+samriddha-basu-cloud@users.noreply.github.com>
2021-04-05 23:55:12 +0530

    Create _fun.java

---
+++ Samriddha/_fun.java
@@ -0,0 +1,47 @@
+java.util.Scanner;
+class Power
+{
+  public static void main(String args[])
+  {
+ 
+    long n,p,r=1;
+ 
+     Scanner sc=new Scanner(System.in);
+ 
+     System.out.println("enter number");
+ 
+     n=sc.nextLong();
+  
+     System.out.println("enter power");
+ 
+     p=sc.nextLong();
+ 
+     r=Power.calpower(n,p);
+ 
+     System.out.println(n+"^"+p+"="+r);
+ 
+  }
+ 
+  static long calpower(long n1,long p1)
+  {
+ 
+     long r1=1;
+ 
+     if(n1>=0&&p1==0)
+     
+        r1 =1;
+ 
+     else if(n1==0&&p1>=1)
+ 
+        r1=0;
+     else
+ 
+       for(int i=1;i<=p1;i++)
+       
+         r1=r1 *n1;
+ 
+     return r1;
+ 
+    }
+ 
+}
