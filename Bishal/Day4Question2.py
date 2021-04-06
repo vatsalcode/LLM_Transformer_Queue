32aa9ef11f07eb7386bce1bb4087a64c8354776f
Bishal Kumar Ghosh <81809899+bISHAL-2001@users.noreply.github.com>
2021-04-06 21:05:51 +0530

    Pattern 4

--- Bishal/Day4Question2.py
+++ Bishal/Day4Question2.py
@@ -3,7 +3,8 @@
 #1  3   5
 #1  3   5   7
 #1  3   5   7   ...
-for i in range(5):
+rows = int(input("Enter the number of rows: "))
+for i in range(rows):
     c = 1
     for j in range(i+1):
         print(c, end=' ')
