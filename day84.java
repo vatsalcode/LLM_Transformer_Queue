d6b262114c015398475e32042d52cbfe55232d57
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2021-04-12 08:52:21 +0530

    Create day84.java

---
+++ day84.java
@@ -0,0 +1,15 @@
+public class SumNatural {
+
+    public static void main(String[] args) {
+
+        int num = 100, sum = 0;
+
+        for(int i = 1; i <= num; ++i)
+        {
+            // sum = sum + i;
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
