b786fbef242fe36bdcbe4f87784bf0ce9390be77
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-04-14 11:36:00 +0530

    Create linked list.py

---
+++ Abhishek Tyagi/linked list.py
@@ -0,0 +1,43 @@
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def printLinkedList(head):
+    if head:                 
+        print(head.data)             
+        printLinkedList(head.next)   
+    
+
+if __name__ == '__main__':
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    printLinkedList(llist.head)
