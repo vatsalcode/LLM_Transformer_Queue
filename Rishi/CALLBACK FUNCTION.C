16fb0ec4fdbb8ec3cc03eb41da5a6d9552795725
Rishi Dhingra <rishidhingra04@gmail.com>
2021-04-20 21:15:43 +0530

    Create CALLBACK FUNCTION.C

---
+++ Rishi/CALLBACK FUNCTION.C
@@ -0,0 +1,18 @@
+#include<stdio.h> 
+ void A() 
+{ 
+    printf("I am function A\n"); 
+} 
+  // callback function 
+void B(void (*ptr)()) 
+{ 
+    (*ptr) (); // callback to A 
+} 
+ int main() 
+{ 
+    void (*ptr)() = &A; 
+    // calling function B and passing 
+    // address of the function A as argument 
+    B(ptr); 
+    return 0; 
+}
