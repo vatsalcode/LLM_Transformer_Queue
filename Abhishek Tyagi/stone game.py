cd9716805444105d87458f58cc4f38c936b09465
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-04-21 10:47:02 +0530

    Create stone game.py

---
+++ Abhishek Tyagi/stone game.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def stoneGame(self, piles):
+        
+        if len(piles) % 2 ==0 or len(piles) == 1:
+            return True
+        
+        dp = [0] * len(piles);
+        for i in reversed(xrange(len(piles))):
+            dp[i] = piles[i]
+            for j in xrange(i+1, len(piles)):
+                dp[j] = max(piles[i] - dp[j], piles[j] - dp[j-1])
+        return dp[-1] >= 0 
+      
+
