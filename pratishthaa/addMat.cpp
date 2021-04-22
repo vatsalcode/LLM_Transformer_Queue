2681a8ba3ec9afc5482e31c79fa1c7649ede691b
Pratishtha <77200165+pratishthaa@users.noreply.github.com>
2021-04-22 18:28:06 +0530

    Create addMat.cpp

---
+++ pratishthaa/addMat.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+using namespace std;
+
+class Matrix
+{
+	int a[10][10],b[10][10],c[10][10],i,j,k,m,n,r,col;
+	public:
+		void set_data();
+		void add_matrix();
+		void dispaly();
+};
+
+void Matrix::set_data()
+{
+	cout<<"Enter the number of rows and columns of 1st matrix : ";
+	cin>>n>>m;
+	cout<<"Enter the elements of 1st matrix : ";
+	for(i = 0 ; i < n ; i++)
+	{
+		for(j = 0 ; j < m ; j++)
+		  cin>>a[i][j];
+	}
+	
+	cout<<endl<<"Enter the number of rows and columns of 2nd matrix : ";
+	cin>>r>>col;
+	cout<<endl<<"Enter the elements of 2nd matrix : ";
+	for(i = 0 ; i < r ; i++)
+	{
+		for(j = 0 ; j < col ; j++)
+		  cin>>b[i][j];
+	}
+}
+
+void Matrix::add_matrix()
+{
+	for(i= 0 ; i < n ; i++)
+	{
+		for(j = 0 ; j < r ; j++)
+		{
+			c[i][j] = a[i][j] + b[i][j];
+		}
+	}
+}
+
+void Matrix::dispaly()
+{
+	cout<<"1st martix is : "<<endl;
+	for(i = 0 ; i < n ; i++)
+	{		
+	    for(j= 0 ; j < m ; j++)
+	    {
+	    	cout<<"\t"<<a[i][j];
+		}
+		cout<<endl;
+	}
+	cout<<"2nd martix is : "<<endl;
+	for(i = 0 ; i < r ; i++)
+	{		
+	    for(j= 0 ; j < col ; j++)
+	    {
+	    	cout<<"\t"<<b[i][j];
+		}
+		cout<<endl;
+	}
+	cout<<"Addition of 2 martices are : "<<endl;
+	for(i = 0 ; i < r ; i++)
+	{		
+	    for(j= 0 ; j < col ; j++)
+	    {
+	    	cout<<"\t"<<c[i][j];
+		}
+		cout<<endl;
+	}
+}
+
+int main()
+{
+	Matrix m;
+	m.set_data();
+	m.add_matrix();
+	m.dispaly();
+	
+	return 0;
+}
