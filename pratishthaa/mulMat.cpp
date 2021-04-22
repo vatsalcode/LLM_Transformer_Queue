16d7667f8495ecf84e75892bba213e8e6d580c40
Pratishtha <77200165+pratishthaa@users.noreply.github.com>
2021-04-22 18:33:11 +0530

    Create mulMat.cpp

---
+++ pratishthaa/mulMat.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+using namespace std;
+
+class Matrix
+{
+	int a[10][10],b[10][10],c[10][10],i,j,k,m,n,r,col;
+	public:
+		void set_data();
+		void mul_matrix();
+		void dispaly();
+};
+
+void Matrix::set_data()
+{
+	cout<<"Enter the number of rows and columns of 1st matrix : ";
+	cin>>n>>m;
+	cout<<"Enter the elements of 1st matrix : ";
+	for(i = 0 ; i < n ; i++)
+	{
+		for(j = 0 ; j < m ; j++)
+		  cin>>a[i][j];
+	}
+	
+	cout<<endl<<"Enter the number of rows and columns of 2nd matrix : ";
+	cin>>r>>col;
+	cout<<endl<<"Enter the elements of 2nd matrix : ";
+	for(i = 0 ; i < r ; i++)
+	{
+		for(j = 0 ; j < col ; j++)
+		  cin>>b[i][j];
+	}
+}
+
+void Matrix::mul_matrix()
+{
+	for(i= 0 ; i < n ; i++)
+	{
+		for(j = 0 ; j < col ; j++)
+		{
+			c[i][j] = 0;
+		   for(k = 0 ; k < m ; k++)
+		   {
+		   	    c[i][j] = c[i][j] + a[i][k] * b[k][j];
+		   }	
+		}
+	}
+}
+
+void Matrix::dispaly()
+{
+	cout<<"1st martix is : "<<endl;
+	for(i = 0 ; i < n ; i++)
+	{		
+	    for(j= 0 ; j < m ; j++)
+	    {
+	    	cout<<"\t"<<a[i][j];
+		}
+		cout<<endl;
+	}
+	cout<<"2nd martix is : "<<endl;
+	for(i = 0 ; i < r ; i++)
+	{		
+	    for(j= 0 ; j < col ; j++)
+	    {
+	    	cout<<"\t"<<b[i][j];
+		}
+		cout<<endl;
+	}
+	cout<<"Multiplication of 2 martices are : "<<endl;
+	for(i = 0 ; i < r ; i++)
+	{		
+	    for(j= 0 ; j < col ; j++)
+	    {
+	    	cout<<"\t"<<c[i][j];
+		}
+		cout<<endl;
+	}
+}
+
+int main()
+{
+	Matrix m;
+	m.set_data();
+	m.mul_matrix();
+	m.dispaly();
+	
+	return 0;
+}
