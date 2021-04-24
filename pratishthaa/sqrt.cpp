fe254e9b6b90e7eb869f939ba3da44b03584c730
Pratishtha <77200165+pratishthaa@users.noreply.github.com>
2021-04-24 18:49:57 +0530

    Create sqrt.cpp

---
+++ pratishthaa/sqrt.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int t,j;
+	cin>>t;
+	while(t--)
+	{
+		int i;
+		cin>>i;
+	    j=sqrt(i);
+	    cout<<j<<endl;
+	}
+	
+	return 0;
+}
