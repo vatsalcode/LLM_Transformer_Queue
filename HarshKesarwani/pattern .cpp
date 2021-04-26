4bba54cef2528b32b2e076d5dc890a9a9e14c8e0
Harsh Kesarwani <77579808+harshkcodes@users.noreply.github.com>
2021-04-26 23:34:08 +0530

    Create pattern .cpp

---
+++ HarshKesarwani/pattern .cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char a = 'A';
+    for (int x = 1; x <= 7; x++)
+    {
+        for (int y = 1; y <= 7; y++)
+        {
+            if (x + y == 11 || x - y == 3 || y - x == 3 || x + y == 5)
+            {
+                cout << a << " ";
+                a++;
+            }
+            else
+            {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
