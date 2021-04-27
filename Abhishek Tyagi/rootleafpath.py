dc3c4c541b13c732165a7234da08d1cd82e07ff3
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-04-27 21:23:26 +0530

    Create rootleafpath.py

---
+++ Abhishek Tyagi/rootleafpath.py
@@ -0,0 +1,25 @@
+class Node(object):
+    def __init__(self, data = None):
+        self.left = None
+        self.right = None
+        self.data = data
+
+def printPath(node, path = []):
+    if node is None:
+        return
+    path.append(node.data)
+
+    if (node.left is None) and (node.right is None):
+        print(' '.join([str(i) for i in path if i != 0]))
+    else:
+        printPath(node.left, path)
+        printPath(node.right, path[0:1])
+
+if __name__ == '__main__':
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.right.left = Node(5)
+
+    printPath(root)
