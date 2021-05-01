b84b142b2ca75b520b5a46fadea9499840ffaf65
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-02 01:58:09 +0530

    Create modular_exponentiation.py

---
+++ Abhishek Tyagi/modular_exponentiation.py
@@ -0,0 +1,13 @@
+def power(x, y, p) : 
+    res = 1     
+    x = x % p  
+  
+    while (y > 0) : 
+          
+        if ((y & 1) == 1) : 
+            res = (res * x) % p 
+  
+        y = y >> 1      
+        x = (x * x) % p 
+          
+    return res 
