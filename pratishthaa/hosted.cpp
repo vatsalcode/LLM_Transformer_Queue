b70b96a780f67ab789d0fac47f2040d3aab122c0
Pratishtha <77200165+pratishthaa@users.noreply.github.com>
2021-05-02 15:24:11 +0530

    Create hosted.cpp

---
+++ pratishthaa/hosted.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int a;
+		cin>>a;
+		if(a==2010||a==2015||a==2016||a==2017||a==2019)
+		{
+			cout<<"HOSTED"<<endl;
+		}
+		else
+		{
+			cout<<"NOT HOSTED"<<endl;
+		}
+		
+	}
+	return 0;
+	
+}
