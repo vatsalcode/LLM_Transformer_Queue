f0251776fc4de2d178a384e7db291bd6af4b7771
Vaibhav Rawal <77624981+vaibhav-r@users.noreply.github.com>
2021-05-06 13:17:31 +0530

    Create day107.java

---
+++ day107.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+
+    // create int variables
+    int a = 25;
+    int b = 34;
+
+    // convert int into long
+    // using typecasting
+    long c = a;
+    long d = b;
+
+    System.out.println(c);    // 25
+    System.out.println(d);    // 34
+  }
+}
