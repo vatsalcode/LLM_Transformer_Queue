d00fbd76fe9eea9c0b8b7be9cd3e421d21d72838
Darpan Khanna <72389408+darpankhanna@users.noreply.github.com>
2021-05-06 19:00:08 +0530

    Create Time delta.py

---
+++ darpanKhanna/Time delta.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+for i in range (int (input())):
+    t1=datetime.strptime(input(),'%a %d %b %Y %H:%M:%S %z')
+    t2=datetime.strptime(input(),'%a %d %b %Y %H:%M:%S %z')
+    print(abs(int((t1-t2).total_seconds())))
