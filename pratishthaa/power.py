f7d6fbe488f74b7da7d7a680c7996b4ffd154c9f
Pratishtha <77200165+pratishthaa@users.noreply.github.com>
2021-05-25 23:51:18 +0530

    Create power.py

---
+++ pratishthaa/power.py
@@ -0,0 +1,6 @@
+terms = int(input("How many terms? "))
+result = list(map(lambda x: 2 ** x, range(terms)))
+
+print("The total terms are:",terms)
+for i in range(terms):
+   print("2 raised to power",i,"is",result[i])
