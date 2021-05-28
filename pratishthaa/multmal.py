1efec5cc4f0774e5243a31dba3c89e3e244d754e
Pratishtha <77200165+pratishthaa@users.noreply.github.com>
2021-05-28 07:30:03 +0530

    Create multmal.py

---
+++ pratishthaa/multmal.py
@@ -0,0 +1,17 @@
+X = [[12,7,3],
+    [4 ,5,6],
+    [7 ,8,9]]
+Y = [[5,8,1,2],
+    [6,7,3,0],
+    [4,5,9,1]]
+result = [[0,0,0,0],
+         [0,0,0,0],
+         [0,0,0,0]]
+
+for i in range(len(X)):
+   for j in range(len(Y[0])):
+       for k in range(len(Y)):
+           result[i][j] += X[i][k] * Y[k][j]
+
+for r in result:
+   print(r)
