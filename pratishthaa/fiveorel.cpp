bc9e894f2def7ac93533b3a0ff1914da2490410e
Pratishtha <77200165+pratishthaa@users.noreply.github.com>
2021-06-05 21:40:45 +0530

    Create fiveorel.cpp

---
+++ pratishthaa/fiveorel.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	if(n%5 ==0 && n%11==0)
+	{
+		cout<<"BOTH"<<endl;
+	}
+	else if(n%5 ==0 || n%11==0)
+	{
+		cout<<"BOTH"<<endl;
+	}
+	else
+	{
+		cout<<"BOTH"<<endl;
+	}
+	return 0;
+}
