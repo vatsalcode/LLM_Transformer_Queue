b46aefa6d2724f54fc989eb47a9791a885d9205a
Pratishtha <77200165+pratishthaa@users.noreply.github.com>
2021-06-16 22:15:53 +0530

    Create srtlssndel.cpp

---
+++ pratishthaa/srtlssndel.cpp
@@ -0,0 +1,9 @@
+a=[['A',33],['B',20],['C',25]]
+for i in range(0,len(a)):
+    for j in range(0,len(a)-i-1):
+        if(a[j][1]>a[j+1][1]):
+            temp=a[j]
+            a[j]=a[j+1]
+            a[j+1]=temp
+ 
+print(a)
