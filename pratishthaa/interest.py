b7ee47e8e786da742bab148c836fb4c2ac039fae
Pratishtha <77200165+pratishthaa@users.noreply.github.com>
2021-06-18 23:07:48 +0530

    Create interest.py

---
+++ pratishthaa/interest.py
@@ -0,0 +1,5 @@
+principle=float(input("Enter the principle amt:"))
+time=int(input("Enter the time(yrs):"))
+rate=float(input("Enter the rate:"))
+simple_interest=(principle*time*rate)/100
+print("The simple interest is:",simple_interest)
