78b7706b9606747c9784646250ee63338024fb17
Pratishtha <77200165+pratishthaa@users.noreply.github.com>
2021-06-19 23:56:55 +0530

    Create grtnine.cpp

---
+++ pratishthaa/grtnine.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if(n>9)
+    {
+        cout<<"Greater than 9";
+    }
+    else if(n==1)
+    {
+        cout<<"one";
+    }
+    else if(n==2)
+    {
+        cout<<"two";
+    }
+    else if(n==3)
+    {
+        cout<<"three";
+    }
+    else if(n==4)
+    {
+        cout<<"four";
+    }
+    else if(n==5)
+    {
+        cout<<"five";
+    }
+    else if(n==6)
+    {
+        cout<<"six";
+    }
+    else if(n==7)
+    {
+        cout<<"seven";
+    }
+    else if(n==8)
+    {
+        cout<<"eight";
+    }
+    else if(n==9)
+    {
+        cout<<"nine";
+    }
+    return 0;
+}
