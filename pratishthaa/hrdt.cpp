37368a8c5a9c806a0af83f30d403a1bec880265e
Pratishtha <77200165+pratishthaa@users.noreply.github.com>
2021-06-19 23:39:51 +0530

    Create hrdt.cpp

---
+++ pratishthaa/hrdt.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long int b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<e<<endl;
+    return 0;
+}
