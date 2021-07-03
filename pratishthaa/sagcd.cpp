efef2226e7e6a3f9734642b1f606c01689ff15a8
Pratishtha <77200165+pratishthaa@users.noreply.github.com>
2021-07-03 22:47:17 +0530

    Create sagcd.cpp

---
+++ pratishthaa/sagcd.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+int gcd(int a, int b)
+{
+	if (a == 0)
+		return b;
+	return gcd(b % a, a);
+}
+
+void chkGCD(int arr[], int n)
+{
+	int result = arr[0];
+	int count=0;
+	int ncount=0;
+	for (int i = 1; i < n; i++)
+	{
+		result = gcd(arr[i], result);
+
+		if(result == 1)
+		{
+			count++;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	if(count>=1)
+	{
+		cout<<count+1<<endl;
+	}
+	else
+	{
+		cout<<"-1"<<endl;
+	}
+}
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>arr[i];
+		}
+		chkGCD(arr,n);
+	}
+}
