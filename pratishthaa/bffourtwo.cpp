b7886433372086446274fd96cdbe367bf98f3b4d
Pratishtha <77200165+pratishthaa@users.noreply.github.com>
2021-07-05 23:40:58 +0530

    Create bffourtwo.cpp

---
+++ pratishthaa/bffourtwo.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int arr[]={1,2,88,42,99};
+	for(int i=0;i<5;i++)
+	{
+		if(arr[i]==42)
+		{
+			break;
+		}
+		else
+		{
+			cout<<arr[i]<<endl;
+		}
+	}
+}
